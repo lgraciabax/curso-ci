2daca12f016e2ad963ba05ecc4c1a77ca6c10b38
LuisEnriqueLopezCruz <luis.lopezc@axity.com>
2024-03-27 16:02:10 -0600

    Corrijo funciones

--- services/cambio-moneda-service/src/routes/currencyRoutes.js
+++ services/cambio-moneda-service/src/routes/currencyRoutes.js
@@ -3,45 +3,30 @@ const router = express.Router();
 const currencyController = require('../controllers/currencyController');
 
 router.get('/convert-to-quetzales', (req, res) => {
-    const { amount, from } = req.query;
+    const { amount } = req.query;
     // Realiza la conversión a quetzales (GTQ)
-    let convertedAmount;
-    if (from === 'USD') {
-        convertedAmount = amount * 7.68; // 1 USD = 7.68 GTQ
-    } else if (from === 'EUR') {
-        convertedAmount = amount * 8.35; // 1 EUR = 8.35 GTQ
-    } else {
-        throw new Error('No se puede convertir desde la moneda especificada a Quetzales');
-    }
-    res.json({ amount, from, to: 'GTQ', convertedAmount });
+    const factorConversion = 0.47;
+    const convertedAmount = amount * factorConversion;
+   
+    res.json({  convertedAmount });
 });
 
 router.get('/convert-to-euros', (req, res) => {
-    const { amount, from } = req.query;
-    // Realiza la conversión a euros (EUR)
-    let convertedAmount;
-    if (from === 'USD') {
-        convertedAmount = amount * 0.92; // 1 USD = 0.92 EUR
-    } else if (from === 'GTQ') {
-        convertedAmount = amount * 0.12; // 1 GTQ = 0.12 EUR
-    } else {
-        throw new Error('No se puede convertir desde la moneda especificada a Euros');
-    }
-    res.json({ amount, from, to: 'EUR', convertedAmount });
+    const { amount } = req.query;
+    // Realiza la conversión a quetzales (GTQ)
+    const factorConversion = 16.54;
+    const convertedAmount = amount / factorConversion;
+   
+    res.json({  convertedAmount });
 });
 
 router.get('/convert-to-usd', (req, res) => {
-    const { amount, from } = req.query;
-    // Realiza la conversión a dólares estadounidenses (USD)
-    let convertedAmount;
-    if (from === 'EUR') {
-        convertedAmount = amount * 1.09; // 1 EUR = 1.09 USD
-    } else if (from === 'GTQ') {
-        convertedAmount = amount * 0.13; // 1 GTQ = 0.13 USD
-    } else {
-        throw new Error('No se puede convertir desde la moneda especificada a Dólares');
-    }
-    res.json({ amount, from, to: 'USD', convertedAmount });
+    const { amount } = req.query;
+    // Realiza la conversión a quetzales (GTQ)
+    const factorConversion = 17.91;
+    const convertedAmount = amount / factorConversion;
+   
+    res.json({  convertedAmount });
 });
 
 
