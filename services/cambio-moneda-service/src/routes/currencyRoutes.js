68f6d21985d5dbdccd28ad228acee65406adff4e
ErikGonzalez11 <165073160+ErikGonzalez11@users.noreply.github.com>
2024-03-27 17:36:50 -0600

    Update

--- services/cambio-moneda-service/src/routes/currencyRoutes.js
+++ services/cambio-moneda-service/src/routes/currencyRoutes.js
@@ -1,32 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const currencyController = require('../controllers/currencyController');
-
-router.get('/convert-to-quetzales', (req, res) => {
+router.get('/convert-to-yuan', (req, res) => {
     const { amount } = req.query;
-   
-    const factorConversion = 0.47;
+
+
+    const factorConversion = 0.44;
+
     const convertedAmount = amount * factorConversion;
-   
-    res.json({  convertedAmount });
-});
 
-router.get('/convert-to-euros', (req, res) => {
-    const { amount } = req.query;
-    
-    const factorConversion = 16.54;
-    const convertedAmount = amount / factorConversion;
-   
-    res.json({  convertedAmount });
-});
-
-router.get('/convert-to-usd', (req, res) => {
-    const { amount } = req.query;
-    
-    const factorConversion = 17.91;
-    const convertedAmount = amount / factorConversion;
-   
-    res.json({  convertedAmount });
+    res.json({ amount, to: 'CNY', convertedAmount });
 });
 
 router.get('/convert-to-yuan', (req, res) => {
