cedd1392627ff5102a29eda8570a1fb5419b8791
Gerardo Antonio Gomez Izquierdo <Gerardo.GomezI@axity.com>
2024-03-27 16:44:34 -0600

    Se agregran cards

--- services/cambio-moneda-front/my-app/src/App.js
+++ services/cambio-moneda-front/my-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import MainCard from './components/MainCard';
 
 function App() {
   return (
     <div className="App">
         <Header />
-        
+        <MainCard />
     </div>
   );
 }
