5a126fafb1fd69c0b56cca692d37f4fe589e6c3c
Erikgb11 <erikgb11@outlook.com>
2024-03-27 16:31:48 -0600

    feat: se agrego la funcion convert-to-yuan

--- services/cambio-moneda-service/src/routes/currencyRoutes.js
+++ services/cambio-moneda-service/src/routes/currencyRoutes.js
@@ -29,5 +29,16 @@ router.get('/convert-to-usd', (req, res) => {
     res.json({  convertedAmount });
 });
 
+router.get('/convert-to-yuan', (req, res) => {
+    const { amount } = req.query;
+
+
+    const factorConversion = 0.44;
+
+    const convertedAmount = amount * factorConversion;
+
+    res.json({ amount, to: 'CNY', convertedAmount });
+});
+
 
 module.exports = router;
